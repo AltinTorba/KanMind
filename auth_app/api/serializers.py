7bf9096022bab3f59cc68a5fbf5f6575c8502205
AltinTorba <alabto@gmail.com>
2026-05-12 20:26:11 +0200

    Setup project structure, Board model, and registration serializer

---
+++ auth_app/api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    fullname = serializers.CharField(write_only=True)
+    repeated_password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["fullname", "email", "password", "repeated_password"]
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["repeated_password"]:
+            raise serializers.ValidationError("Passwords do not match")
+        return attrs
+
+    def create(self, validated_data):
+        fullname = validated_data["fullname"]
+        password = validated_data["password"]
+
+        user = User(
+            username=fullname,
+            email=validated_data["email"]
+        )
+
+        user.set_password(password)
+        user.save()
+        return user
